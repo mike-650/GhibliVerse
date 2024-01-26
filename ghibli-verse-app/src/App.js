3672c9b79d754c13c06acf3c53a7b181132f120b
Michael <salariosamichael@gmail.com>
2024-01-26 15:59:38 -0800

    initialized react-app

---
+++ ghibli-verse-app/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <div>GhibliVerse!</div>
+  );
+}
+
+export default App;
---
+++ ghibli-verse-app/src/Components/Navigation.js
@@ -0,0 +1,8 @@
+function Navigation() {
+  return (
+    <h1>Navigation Bar</h1>
+  )
+}
+
+
+export default Navigation
