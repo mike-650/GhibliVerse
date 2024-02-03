f382275215bc8d2c2287ebf7650c5cff08ee565b
MichaelSal <salariosamichael@gmail.com>
2024-02-02 22:41:57 -0800

    working on implementing video preview on main carousel

--- ghibli-verse-app/src/components/Home/Carousel.js
+++ ghibli-verse-app/src/components/Home/Carousel.js
@@ -1,6 +1,7 @@
 import image1 from './images/spirited-away.jpg';
 import image2 from './images/princessmononoke.jpg';
 import image3 from './images/ponyo.jpg';
+import video from './videos/spirited-away.mp4'
 
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
@@ -14,15 +15,20 @@ function Carousel() {
         navigation='true'
         pagination='true'
         loop='true'
-        autoplay='{"delay":3000}'
+        // autoplay='{"delay":5000}'
         // UNCOMMENT TO ACTIVATE AUTOPLAY
       >
         <swiper-slide>
           <div>
-            <img
+            {/* <img
               src={image1}
               alt='First carousel studio ghibli film'
-            />
+              className='cursor-pointer'
+            /> */}
+            <video autoPlay muted className='w-full'>
+              <source src={video} type="video/mp4"/>
+              "Your browser does not support the video tag."
+            </video>
             <div className="absolute top-0 left-20 bottom-0 flex items-center justify-center w-80 bg-gray-900 bg-opacity-75">
               <p className="text-white text-2xl font-playfair font-bold">Spirited Away</p>
             </div>
@@ -33,6 +39,7 @@ function Carousel() {
             <img
               src={image2}
               alt='First carousel studio ghibli film'
+              className='cursor-pointer'
             />
             <div className="absolute top-0 left-20 bottom-0 flex items-center justify-center w-80 bg-gray-900 bg-opacity-75">
               <p className="text-white text-2xl font-playfair font-bold">Princess Mononoke</p>
@@ -44,6 +51,7 @@ function Carousel() {
             <img
               src={image3}
               alt='First carousel studio ghibli film'
+              className='cursor-pointer'
             />
             <div className="absolute top-0 left-20 bottom-0 flex items-center justify-center w-80 bg-gray-900 bg-opacity-75">
               <p className="text-white text-2xl font-playfair font-bold">Ponyo</p>
